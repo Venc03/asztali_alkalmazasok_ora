1ac91ffcd717179fcc63187b31a8ea1432826687
Venc03 <vegi.dani1@gmail.com>
2023-02-22 12:29:25 +0100

    masodik feladat

--- Ora230222.java
+++ Ora230222.java
@@ -1,26 +1,33 @@
 package ora230222;
-public class Ora230222 {
-    
-    public static void main(String[] args) {
-        
 
+public class Ora230222 {
+
+    public static void main(String[] args) {
+
+        int a = 3;
+        int b = 5;
+        int elso10Osszeg = elso10SzamOsszege();
+        kiir(String.format("Elso tiz szam osszege: %d", elso10Osszeg));
+        int abOsszeg = osszead(a, b);
+        kiir(String.format("%d + %d = %d", a, b, abOsszeg));
+        
     }
-    
-    private static int elso10SzamOsszege(int max) {
+
+    private static int elso10SzamOsszege() {
         int osszeg = 0;
-        for (int i = 0; i < max; i++) {
+        for (int i = 0; i < 10; i++) {
             osszeg += i;
         }
         return osszeg;
+
     }
-    
+
     private static int osszead(int a, int b) {
         return a + b;
     }
-    
+
     private static void kiir(String szoveg) {
         System.out.println(szoveg);
     }
 
-    
 }
