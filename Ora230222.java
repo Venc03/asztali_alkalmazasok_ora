b01321811e959c6091133a6df0292e9571b14b50
Venc03 <vegi.dani1@gmail.com>
2023-02-22 12:03:30 +0100

    elso feladat

--- Ora230222.java
+++ Ora230222.java
@@ -1,6 +1,35 @@
 package ora230222;
 public class Ora230222 {
+    
     public static void main(String[] args) {
+        
+        
+        
     }
+    
+    private static void xSzamOssz() {
+        int x = 10;
+        int ossz = xSzamOssz(x);
+        kiiras("Elso %d szam összeg: %d\n".formatted(x, ossz));
+    }
+    
+    private static int xSzamOssz(int max) {
+        int ossz = 0;
+        for (int i = 0; i < max; i++) {
+            ossz += i;
+            
+        }
+        return ossz;
+    }
+    
+    private static void szamolas(int a, int b) {
+        int ossz = a + b;
+        kiiras("%d + %d = %d\n".formatted(a, b, ossz));
+    }
+    
+    private static void kiiras(String szov) {
+        System.out.println(szov);
+    }
+
     
 }
