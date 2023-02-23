148e15a27cb2b1951d74fddbc472f44d56c85ecc
Venc03 <63917996+Venc03@users.noreply.github.com>
2023-02-23 12:58:11 +0100

    sorvegeszoveg

--- Equalizer.java
+++ Equalizer.java
@@ -3,6 +3,8 @@ package ora230222;
 import java.util.Random;
 
 public class Equalizer {
+    private static final String White = "\u001B[0m";
+    private static final String Purple = "\u001B[45m";
     private static final Random rnd = new Random();
     
     public static void main(String[] args) {
@@ -14,26 +16,36 @@ public class Equalizer {
     
      private static void program() {
         eq();
+        eq(8, true);
         eq(12);
-        eq();
-        eq(8);
+        eq(true);
         eq();
     }
 
     private static void eq(int hossz) {
-        System.out.println(blokkGeneralas(hossz));
+        eq(false);
     }
+    
     private static void eq() {
         System.out.println(blokkGeneralas(rnd.nextInt(3, 8)));
+    }
+   
+    private static void eq(int hossz, boolean hosszKiir) {
+        String hosszSzoveg = blokkGeneralas(hossz);
+        hosszSzoveg += (hosszKiir ? "%s (%d)".formatted(White, hossz) : "");
+        System.out.println(hosszSzoveg);
+    }
+    
+    private static void eq(boolean hosszKiir) {
+        eq(rnd.nextInt(3, 8), hosszKiir);
     }
 
     private static String blokkGeneralas(int hossz) {
         String szov = "";
         for (int i = 0; i < hossz; i++) {
-            szov += "\u001B[45m" + " ";
-            
+            szov += Purple + " ";        
         }
         return szov;
     }
-    
+   
 }
